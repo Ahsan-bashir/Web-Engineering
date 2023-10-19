ab20905631c5ec7a1a553fad38324e170d225c36
Ahsan-bashir <bteeccc@gmail.com>
2023-10-19 23:20:55 +0500

    completed project setup for tailwind (lab 5)

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {     
+    extend: {
+      colors:{
+        ahsan:"#f00946",
+      }
+    },
+   },
+  plugins: [],
+}
+
